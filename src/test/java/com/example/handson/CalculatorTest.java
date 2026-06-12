5fbdae7a067e2dfa282e6701be186669a920895d
noatakahashi <noa.takahashi@tcs.com>
2026-06-12 13:50:00 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -10,28 +10,24 @@ public class CalculatorTest {
     @Test
     public void testAddIntegerValues() {
         Calculator calc = new Calculator();
-
         assertEquals(5.0, calc.add(2.0, 3.0));
     }
 
     @Test
     public void testAddDecimalValues() {
         Calculator calc = new Calculator();
-
         assertEquals(4.7, calc.add(1.2, 3.5), 0.0001);
     }
 
     @Test
     public void testAddThreeValues() {
         Calculator calc = new Calculator();
-
         assertEquals(6.0, calc.add(1.0, 2.0, 3.0));
     }
 
     @Test
     public void testAddThreeValuesWhenAIsLessThanZero() {
         Calculator calc = new Calculator();
-
         assertThrows(IllegalArgumentException.class, () -> {
             calc.add(-1.0, 2.0, 3.0);
         });
@@ -40,7 +36,6 @@ public class CalculatorTest {
     @Test
     public void testAddThreeValuesWhenBIsGreaterThanTen() {
         Calculator calc = new Calculator();
-
         assertThrows(IllegalArgumentException.class, () -> {
             calc.add(1.0, 11.0, 3.0);
         });
@@ -49,9 +44,16 @@ public class CalculatorTest {
     @Test
     public void testAddThreeValuesWhenCIsGreaterThanTen() {
         Calculator calc = new Calculator();
-
         assertThrows(IllegalArgumentException.class, () -> {
             calc.add(1.0, 2.0, 11.0);
         });
     }
 
+    @Test
+    public void testAddThreeValuesAtBoundaryValues() {
+        Calculator calc = new Calculator();
+        assertEquals(30.0, calc.add(10.0, 10.0, 10.0));
+        assertEquals(0.0, calc.add(0.0, 0.0, 0.0));
+    }
+
+}
