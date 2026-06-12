0d2eb57d835a5d807909c13ffdbf4021f37448f7
noatakahashi <noa.takahashi@tcs.com>
2026-06-12 13:22:36 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -1,10 +1,16 @@
 package com.example.handson;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
-@Test
-public void testAdd() {
-Calculator calc = new Calculator();
-assertEquals(5, calc.add(2, 3));
-}
+
+    @Test
+    public void testAddIntegerValues() {
+        Calculator calc = new Calculator();
+
+        assertEquals(5.0, calc.add(2.0, 3.0));
+    }
+
 }
